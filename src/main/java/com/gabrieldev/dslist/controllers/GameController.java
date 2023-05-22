55c161009ad6403d2daf8ca74f9b9a6a3c6d5469
GabrielOliveira04 <107278235+GabrielOliveira04@users.noreply.github.com>
2023-05-21 21:13:08 -0300

    Domain model

--- src/main/java/com/gabrieldev/dslist/controllers/GameController.java
+++ src/main/java/com/gabrieldev/dslist/controllers/GameController.java
@@ -1,14 +1,13 @@
 package com.gabrieldev.dslist.controllers;
 
-import java.util.List;
-
+import com.gabrieldev.dslist.dto.GameMinDTO;
+import com.gabrieldev.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gabrieldev.dslist.dto.GameMinDTO;
-import com.gabrieldev.dslist.services.GameService;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/games")
